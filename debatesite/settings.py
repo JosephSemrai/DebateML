de3428f7d8d931395d4b65dad4391846ce785712
Joseph Semrai <josephsemrai@gmail.com>
2019-07-20 19:13:12 -0400

    Initial production test

--- debatesite/settings.py
+++ debatesite/settings.py
@@ -15,6 +15,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# Quick fix, look to https://stackoverflow.com/questions/36665889/collectstatic-error-while-deploying-django-app-to-heroku
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
